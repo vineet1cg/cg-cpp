6f034f458af8c729fc00bb636c5287e3a7e20c82
vineet1cg <vineet.prajapati.cg@gmail.com>
2026-02-24 10:03:33 +0530

    add OOPS definitions

---
+++ code/OOPS/01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // helps us create a structure so that we dont have to repeat our code everysingle time for each user 
+    //:P
+    // more like a blueprint for a set of data or collection 
+    // similar to schema in databases
+    // platform independent envirornment
+    // mostly same in all the other languages
+    // class =>> creating structure for an object
+    // object =>> collection of data structure for a single entity
+    // In object-oriented programming (OOP), a class is a blueprint or template used to create objects. It defines the common structure (attributes) and behavior (methods) that all objects of a specific type will share.
+    
+    cout<<"HELLO OOPS!"<<endl;
+    return 0;
+}
+
